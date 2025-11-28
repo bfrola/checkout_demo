b4269f9b6d1a40b61584bfdf1b103b2215c9b557
Bernardino Frola <ber.frola@gmail.com>
2025-11-28 11:37:25 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+ import React from 'react'
 import Checkout from './Checkout'
 
 export default function App() {
   return (
     <div className="app">
       <header className="header">
-        <h1>Facebook Commerce Checkout Landing</h1>
-        <p className="subtitle">Demo: lists URL params and parses the <code>products</code> param</p>
+        <h1>Checkout page</h1>
       </header>
       <main>
         <Checkout />
